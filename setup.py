6f7cad3fa1bc194cb14fbf5181c9c80d9bd1e2f3
Adam Hopkins <admhpkns@gmail.com>
2017-08-29 11:07:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,12 +19,14 @@ setuptools.setup(
     tests_require=['pytest', ],
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
